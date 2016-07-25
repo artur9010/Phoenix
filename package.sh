193ff173187dd35b1df93007a231439f135e460e
wvffle <casper@wvffle.net>
2016-07-25 15:08:03 +0200

    Hail to the Waffles!

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
 rm -rf dist
 mkdir dist
 rm -rf Phoenix-*
-electron-packager . --platform=darwin --arch=x64 --asar
-electron-packager . --platform=linux --arch=ia32 --asar
-electron-packager . --platform=win32 --arch=ia32 --asar
+electron-packager . --platform=darwin --arch=x64 --asar --icon icons/icon.icns
+electron-packager . --platform=linux --arch=ia32 --asar --icon icons/icon.png
+electron-packager . --platform=win32 --arch=ia32 --asar --icon icons/icon.ico
 mv Phoenix-darwin-x64/ dist/
 mv Phoenix-linux-ia32/ dist/
 mv Phoenix-win32-ia32/ dist/
