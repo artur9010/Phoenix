edb3dbd35ad403f19c69e9c22c9bfb882c55c6fc
artur9010 <arturmotyka99@gmail.com>
2016-07-26 17:04:54 +0200

    [package.sh] Add windows version options

--- package.sh
+++ package.sh
@@ -4,4 +4,4 @@ mkdir dist
 rm -rf Phoenix-*
 electron-packager . Phoenix --platform=darwin --arch=x64 --asar --icon icons/icon.icns --out=dist --prune app-category-type=public.app-category.utilities
 electron-packager . Phoenix --platform=linux --arch=ia32 --asar --icon icons/icon.png --out=dist --prune
-electron-packager . Phoenix --platform=win32 --arch=ia32 --asar --icon icons/icon.ico --out=dist --prune
+electron-packager . Phoenix --platform=win32 --arch=ia32 --asar --icon icons/icon.ico --out=dist --prune --version-string.ProductName="Phoenix" --version-string.CompanyName="artur9010"
