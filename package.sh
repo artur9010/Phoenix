2e036b0f19d62fc4299e8b8db3836ec272082e81
artur9010 <arturmotyka99@gmail.com>
2016-07-26 16:54:19 +0200

    [package.sh] Add dist directory

--- package.sh
+++ package.sh
@@ -1,9 +1,7 @@
+#!/usr/bin/env bash
 rm -rf dist
 mkdir dist
 rm -rf Phoenix-*
-electron-packager . --platform=darwin --arch=x64 --asar --icon icons/icon.icns
-electron-packager . --platform=linux --arch=ia32 --asar --icon icons/icon.png
-electron-packager . --platform=win32 --arch=ia32 --asar --icon icons/icon.ico
-mv Phoenix-darwin-x64/ dist/
-mv Phoenix-linux-ia32/ dist/
-mv Phoenix-win32-ia32/ dist/
+electron-packager . Phoenix --platform=darwin --arch=x64 --asar --icon icons/icon.icns --out=dist
+electron-packager . Phoenix --platform=linux --arch=ia32 --asar --icon icons/icon.png --out=dist
+electron-packager . Phoenix --platform=win32 --arch=ia32 --asar --icon icons/icon.ico --out=dist
