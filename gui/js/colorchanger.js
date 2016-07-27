d31317e92a5e666232d2f817c682d3ee2a324961
artur9010 <arturmotyka99@gmail.com>
2016-07-27 20:03:27 +0200

    [Phoenix] Remove unused functions

--- gui/js/colorchanger.js
+++ gui/js/colorchanger.js
@@ -24,17 +24,7 @@ waff.qq(".color-block").forEach(function (el) {
     });
 });
 
-//Utils
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
+//Form
 waff.q("#login-form").on("submit", function(e){
     e.preventDefault();
     login_to_messenger();
