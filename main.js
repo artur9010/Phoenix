e6c7cd6ea58d6fc46ca595498a52be79078c1631
artur9010 <arturmotyka99@gmail.com>
2016-07-27 20:33:28 +0200

    [Phoenix] SEMICOLONS!

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
@@ -14,10 +14,10 @@ function createWindow () {
     width: 470,
     height: 600,
     resizable: false
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/gui/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/gui/index.html`);
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
@@ -34,7 +34,7 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -43,7 +43,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
@@ -51,4 +51,4 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
