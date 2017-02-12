aa6a98f249315c74941cfaac1a452a202419860e
wvffle <casper@wvffle.net>
2017-02-12 13:51:10 +0100

    Fixed packaging
    Signed-off-by: wvffle <casper@wvffle.net>

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 rm -rf dist
 mkdir dist
 rm -rf Phoenix-*
-electron-packager . Phoenix --platform=darwin --arch=x64 --asar --icon icons/icon.icns --out=dist --prune app-category-type=public.app-category.utilities
-electron-packager . Phoenix --platform=linux --arch=ia32 --asar --icon icons/icon.png --out=dist --prune
-electron-packager . Phoenix --platform=win32 --arch=ia32 --asar --icon icons/icon.ico --out=dist --prune --version-string.ProductName="Phoenix" --version-string.CompanyName="artur9010"
+node_modules/.bin/electron-packager . Phoenix --platform=darwin --arch=x64 --asar --icon icons/icon.icns --out=dist --prune app-category-type=public.app-category.utilities
+node_modules/.bin/electron-packager . Phoenix --platform=linux --arch=ia32 --asar --icon icons/icon.png --out=dist --prune
+node_modules/.bin/electron-packager . Phoenix --platform=win32 --arch=ia32 --asar --icon icons/icon.ico --out=dist --prune --version-string.ProductName="Phoenix" --version-string.CompanyName="artur9010"
